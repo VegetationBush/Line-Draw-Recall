c83bd0c2f7a6acb943f8f59bf0836124cb573eab
Hans Yang <hans.yangchenghao@gmail.com>
2026-05-24 16:47:02 -0400

    draw

--- frontend/src/components/GameBoard.tsx
+++ frontend/src/components/GameBoard.tsx
@@ -2,15 +2,21 @@ import { useEffect, useRef } from "react"
 
 const CANVAS_SIZE = 100
 function GameBoard() {
-  const drawCanvasRef = useRef<HTMLCanvasElement | null>(null);
+  const drawCanvasRef = useRef<HTMLCanvasElement | null>(null); // for user draw
+  const displayCanvasRef = useRef<HTMLCanvasElement | null>(null); // for prompt/answer
 
   useEffect(() => {
-    const drawCanvas = drawCanvasRef.current
+    const drawCanvas = drawCanvasRef.current;
+    const displayCanvas = displayCanvasRef.current;
 
-    if (!drawCanvas) return;
+    if (!drawCanvas || !displayCanvas) return;
 
     const drawContext = drawCanvas.getContext("2d") as CanvasRenderingContext2D;
-    drawContext.fillStyle = "white"
+    const displayContext = displayCanvas.getContext("2d") as CanvasRenderingContext2D;
+    displayContext.fillStyle = "orange";
+    drawContext.fillStyle = "blue";
+
+    displayContext.fillRect(20, 20, 30, 30);
 
     // draws a 3x3 box around (floor(x), floor(y))
     const drawPoint = (x: number, y: number) => {
@@ -49,6 +55,7 @@ function GameBoard() {
     let isDrawing = false
 
     drawCanvas.addEventListener("mousemove", (e) => {
+      console.log("move")
       if (!isDrawing) return;
 
       drawBrush(e, true)
@@ -66,14 +73,10 @@ function GameBoard() {
 
   return (
     <div style = {{
-      display: "flex",
-
-      alignItems: "center",
-      justifyContent: "center",
-
       height: "100vh",
       width: "100vw",
 
+      isolation: "isolate",
       backgroundColor: "white",
     }}>
       <canvas
@@ -81,12 +84,35 @@ function GameBoard() {
         width = {CANVAS_SIZE}
         height = {CANVAS_SIZE}
         style = {{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+
           height: "calc(min(80vh, 80vw))",
           aspectRatio: "1 / 1",
-          backgroundColor: "black",
-          color: "white",
+          outline: "4px solid blue",
 
           imageRendering: "pixelated",
+        }}>
+      </canvas>
+      <canvas
+        ref = {displayCanvasRef}
+        width = {CANVAS_SIZE}
+        height = {CANVAS_SIZE}
+        style = {{
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+          
+          height: "calc(min(80vh, 80vw))",
+          aspectRatio: "1 / 1",
+          outline: "2px solid black",
+
+          pointerEvents: "none",
+          imageRendering: "pixelated",
+          mixBlendMode: "exclusion",
         }}>
       </canvas>
     </div>
